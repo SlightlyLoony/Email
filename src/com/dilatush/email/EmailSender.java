c42bcd75bfd5b58d885243ef1f3085f62d2712fb
tom <tom@dilatush.com>
2023-04-08 14:08:59 -0600

    minor fixes...

--- src/com/dilatush/email/EmailSender.java
+++ src/com/dilatush/email/EmailSender.java
@@ -14,6 +14,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
@@ -342,9 +343,5 @@ public class EmailSender {
             msg.setRecipients( Message.RecipientType.BCC, _bcc );
         msg.setSubject( _message.subject(), "UTF-8" );
         return msg;
-    }
-
-
-    public static void main( final String[] _args ) throws AddressException, IOException {
     }
 }
